516f4ce74114c1cadf2d36dca7d502eae0fe11a3
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-06-21 15:58:49 -0400

    Added Customers Table with CRUD

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -10,12 +10,34 @@ import {
   Toolbar,
   Sort,
   Filter,
+  Search,
 } from "@syncfusion/ej2-react-grids";
 import { customersData, customersGrid } from "../data/dummy";
 import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={["Search", "Delete"]}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Page, Toolbar, Selection, Edit, Sort, Filter, Search]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
